0a7dd70c8430a522891f8d6525ccac559d4e6ea7
Matti Tahvonen <matti@vaadin.com>
2024-02-25 13:16:34 +0200

    Ugly hack to make sizes work with captions

--- src/main/java/org/vaadin/addons/maplibre/AbstractFeatureField.java
+++ src/main/java/org/vaadin/addons/maplibre/AbstractFeatureField.java
@@ -1,9 +1,7 @@
 package org.vaadin.addons.maplibre;
 
 import com.vaadin.flow.component.customfield.CustomField;
-import org.locationtech.jts.geom.Geometry;
-import org.locationtech.jts.geom.GeometryCollection;
-import org.locationtech.jts.geom.Polygon;
+import com.vaadin.flow.component.dependency.StyleSheet;
 
 public abstract class AbstractFeatureField<T> extends CustomField<T> {
     DrawControl drawControl;
@@ -27,9 +25,13 @@ public abstract class AbstractFeatureField<T> extends CustomField<T> {
     @Override
     public void setHeight(String height) {
         super.setHeight(height);
-        // TODO fix relative sizes
-//        map.setHeightFull();
-        map.setHeight("280px");
+        // TODO figure out how to do this properly, can't be right...
+        // make the custom field's input slot take all available space
+        getElement().executeJs("""
+            var el = this;
+            el.shadowRoot.querySelector(".inputs-wrapper").style.flexGrow = 1;
+        """);
+        map.setHeightFull();
     }
 
     @Override
